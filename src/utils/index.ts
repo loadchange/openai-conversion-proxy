7be29729fb5b7bca334151b4e52beb5ff6f47e8b
Asa <soocto@gmail.com>
2024-04-17 12:43:16 +0000

    Update Prettier configuration and OpenAI proxy code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,24 +8,24 @@ export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve
  * @returns A mapping of the generative model names to the actual model names
  */
 export const generativeModelMappings = (GPT35_TURBO: string, GPT4_TURBO?: string, otherMapping?: any) => {
-	if (!GPT4_TURBO) GPT4_TURBO = GPT35_TURBO;
-	return {
-		'gpt-4-0125-preview': GPT4_TURBO,
-		'gpt-4-turbo-preview': GPT4_TURBO,
-		'gpt-4-1106-preview': GPT4_TURBO,
-		'gpt-4': GPT4_TURBO,
-		'gpt-4-0613': GPT4_TURBO,
-		'gpt-4-32k': GPT4_TURBO,
-		'gpt-4-32k-0613': GPT4_TURBO,
-		'gpt-3.5-turbo-0125': GPT35_TURBO,
-		'gpt-3.5-turbo': GPT35_TURBO,
-		'gpt-3.5-turbo-1106': GPT35_TURBO,
-		'gpt-3.5-turbo-instruct': GPT35_TURBO,
-		'gpt-3.5-turbo-16k': GPT35_TURBO,
-		'gpt-3.5-turbo-0613': GPT35_TURBO,
-		'gpt-3.5-turbo-16k-0613': GPT35_TURBO,
-		...otherMapping,
-	};
+  if (!GPT4_TURBO) GPT4_TURBO = GPT35_TURBO;
+  return {
+    'gpt-4-0125-preview': GPT4_TURBO,
+    'gpt-4-turbo-preview': GPT4_TURBO,
+    'gpt-4-1106-preview': GPT4_TURBO,
+    'gpt-4': GPT4_TURBO,
+    'gpt-4-0613': GPT4_TURBO,
+    'gpt-4-32k': GPT4_TURBO,
+    'gpt-4-32k-0613': GPT4_TURBO,
+    'gpt-3.5-turbo-0125': GPT35_TURBO,
+    'gpt-3.5-turbo': GPT35_TURBO,
+    'gpt-3.5-turbo-1106': GPT35_TURBO,
+    'gpt-3.5-turbo-instruct': GPT35_TURBO,
+    'gpt-3.5-turbo-16k': GPT35_TURBO,
+    'gpt-3.5-turbo-0613': GPT35_TURBO,
+    'gpt-3.5-turbo-16k-0613': GPT35_TURBO,
+    ...otherMapping,
+  };
 };
 
 /**
@@ -34,36 +34,45 @@ export const generativeModelMappings = (GPT35_TURBO: string, GPT4_TURBO?: string
  * @returns
  */
 export const models = async (modelsMapping: any) =>
-	new Response(
-		JSON.stringify({
-			object: 'list',
-			data: Object.keys(modelsMapping).map((id) => ({
-				id,
-				object: 'model',
-				created: 1710035972,
-				owned_by: 'openai',
-				permission: [
-					{
-						id: 'modelperm-M56FXnG1AsIr3SXq8BYPvXJA',
-						object: 'model_permission',
-						created: 1709949572,
-						allow_create_engine: false,
-						allow_sampling: true,
-						allow_logprobs: true,
-						allow_search_indices: false,
-						allow_view: true,
-						allow_fine_tuning: false,
-						organization: '*',
-						group: null,
-						is_blocking: false,
-					},
-				],
-				root: id,
-				parent: null,
-			})),
-		}),
-		{ headers: { 'Content-Type': 'application/json' } }
-	);
+  new Response(
+    JSON.stringify({
+      object: 'list',
+      data: Object.keys(modelsMapping).map((id) => ({
+        id,
+        object: 'model',
+        created: 1710035972,
+        owned_by: 'openai',
+        permission: [
+          {
+            id: 'modelperm-M56FXnG1AsIr3SXq8BYPvXJA',
+            object: 'model_permission',
+            created: 1709949572,
+            allow_create_engine: false,
+            allow_sampling: true,
+            allow_logprobs: true,
+            allow_search_indices: false,
+            allow_view: true,
+            allow_fine_tuning: false,
+            organization: '*',
+            group: null,
+            is_blocking: false,
+          },
+        ],
+        root: id,
+        parent: null,
+      })),
+    }),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+export const gatherResponse = async (response: Response) => {
+  const { headers } = response;
+  const contentType = headers.get('content-type') || '';
+  if (contentType.includes('application/json')) {
+    return await response.json();
+  }
+  return response.text();
+};
 
 /**
  * Array nonempty
@@ -80,3 +89,32 @@ export const isNotEmpty = <T>(arr: any): arr is T[] => Array.isArray(arr) && arr
  * @returns
  */
 export const listLen = (obj: any): number => (isNotEmpty(obj) ? obj.length : 0);
+
+/**
+ * safe JSON parse
+ * @param str
+ * @returns
+ */
+export const JSONParse = (str: string) => {
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    return Object.create(null);
+  }
+};
+
+export function isJSON(str: any) {
+  if (typeof str !== 'string') {
+    return false;
+  }
+  str = str.trim();
+  if ((str.startsWith('{') && str.endsWith('}')) || (str.startsWith('[') && str.endsWith(']'))) {
+    try {
+      JSON.parse(str);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  return false;
+}
